7de65282a9c20dc6fa9bf23a68cb75d09e6efa12
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2021-10-31 10:56:18 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperationAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Math Operation");
+            Console.ReadLine();
+
+        }
+    }
+}
